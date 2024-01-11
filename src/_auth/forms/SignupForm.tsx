d30218e6b1f4ab726389e9afc749976c60b5aedf
Nadein Michael <nadein.michael@gmail.com>
2024-01-11 15:39:13 +0700

    fix warnings

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,9 +1,9 @@
-import { Link, useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useToast } from "@/components/ui/use-toast";
-import { SignupValidation } from "@/lib/validation";
+import { Link, useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useToast } from '@/components/ui/use-toast';
+import { SignupValidation } from '@/lib/validation';
 
 import {
   Form,
@@ -12,36 +12,35 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
-import Loader from "@/components/shared/Loader";
+import Loader from '@/components/shared/Loader';
 import {
   useCreateUserAccount,
   useSignInAccount,
-} from "@/lib/react-query/queriesAndMutations";
-import { useUserContext } from "@/context/AuthContext";
+} from '@/lib/react-query/queriesAndMutations';
+import { useUserContext } from '@/context/AuthContext';
 
 const SignupForm = () => {
   const navigate = useNavigate();
   const { toast } = useToast();
-  const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
+  const { checkAuthUser } = useUserContext();
 
   const { mutateAsync: createUserAccount, isPending: isCreatingAccount } =
     useCreateUserAccount();
 
-  const { mutateAsync: signInAccount, isPending: isSigningIn } =
-    useSignInAccount();
+  const { mutateAsync: signInAccount } = useSignInAccount();
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignupValidation>>({
     resolver: zodResolver(SignupValidation),
     defaultValues: {
-      name: "",
-      username: "",
-      email: "",
-      password: "",
+      name: '',
+      username: '',
+      email: '',
+      password: '',
     },
   });
 
@@ -51,7 +50,7 @@ const SignupForm = () => {
 
     if (!newUser) {
       return toast({
-        title: "Sign up failed. Please try again.",
+        title: 'Sign up failed. Please try again.',
       });
     }
 
@@ -61,42 +60,42 @@ const SignupForm = () => {
     });
 
     if (!session) {
-      return toast({ title: "Sign in failed. Please try again." });
+      return toast({ title: 'Sign in failed. Please try again.' });
     }
 
     const isLoggedIn = await checkAuthUser();
 
     if (isLoggedIn) {
       form.reset();
-      navigate("/");
+      navigate('/');
     } else {
-      return toast({ title: "Sign up failed. Please try again." });
+      return toast({ title: 'Sign up failed. Please try again.' });
     }
   }
 
   return (
     <Form {...form}>
-      <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/logo.svg" />
+      <div className='sm:w-420 flex-center flex-col'>
+        <img src='/assets/images/logo.svg' />
 
-        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+        <h2 className='h3-bold md:h2-bold pt-5 sm:pt-12'>
           Create a new account
         </h2>
-        <p className="text-light-3 small-medium md:base-regular mt-2">
+        <p className='text-light-3 small-medium md:base-regular mt-2'>
           To use Snapgram, please enter your details
         </p>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
-          className="flex flex-col gap-5 w-full mt-4"
+          className='flex flex-col gap-5 w-full mt-4'
         >
           <FormField
             control={form.control}
-            name="name"
+            name='name'
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Name</FormLabel>
                 <FormControl>
-                  <Input type="text" className="shad-input" {...field} />
+                  <Input type='text' className='shad-input' {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -104,12 +103,12 @@ const SignupForm = () => {
           />
           <FormField
             control={form.control}
-            name="username"
+            name='username'
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Username</FormLabel>
                 <FormControl>
-                  <Input type="text" className="shad-input" {...field} />
+                  <Input type='text' className='shad-input' {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -117,12 +116,12 @@ const SignupForm = () => {
           />
           <FormField
             control={form.control}
-            name="email"
+            name='email'
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Email</FormLabel>
                 <FormControl>
-                  <Input type="email" className="shad-input" {...field} />
+                  <Input type='email' className='shad-input' {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -130,32 +129,32 @@ const SignupForm = () => {
           />
           <FormField
             control={form.control}
-            name="password"
+            name='password'
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Password</FormLabel>
                 <FormControl>
-                  <Input type="password" className="shad-input" {...field} />
+                  <Input type='password' className='shad-input' {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
-          <Button type="submit" className="shad-button_primary">
+          <Button type='submit' className='shad-button_primary'>
             {isCreatingAccount ? (
-              <div className="flex-center gap-2">
+              <div className='flex-center gap-2'>
                 <Loader /> Loading...
               </div>
             ) : (
-              "Sign up"
+              'Sign up'
             )}
           </Button>
 
-          <p className="text-small-regular text-light-2 text-center mt-2">
+          <p className='text-small-regular text-light-2 text-center mt-2'>
             Already have an account?
             <Link
-              to="/sign-in"
-              className="text-primary-500 text-small-semibold ml-1"
+              to='/sign-in'
+              className='text-primary-500 text-small-semibold ml-1'
             >
               Log in
             </Link>
--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,24 +1,20 @@
-import Loader from "@/components/shared/Loader";
-import PostCard from "@/components/shared/PostCard";
-import { useGetRecentPosts } from "@/lib/react-query/queriesAndMutations";
-import { Models } from "appwrite";
+import Loader from '@/components/shared/Loader';
+import PostCard from '@/components/shared/PostCard';
+import { useGetRecentPosts } from '@/lib/react-query/queriesAndMutations';
+import { Models } from 'appwrite';
 
 const Home = () => {
-  const {
-    data: posts,
-    isPending: isPostsLoading,
-    isError: isErrorPosts,
-  } = useGetRecentPosts();
+  const { data: posts, isPending: isPostsLoading } = useGetRecentPosts();
 
   return (
-    <div className="flex flex-1">
-      <div className="home-container">
-        <div className="home-posts">
-          <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+    <div className='flex flex-1'>
+      <div className='home-container'>
+        <div className='home-posts'>
+          <h2 className='h3-bold md:h2-bold text-left w-full'>Home Feed</h2>
           {isPostsLoading && !posts ? (
             <Loader />
           ) : (
-            <ul className="flex flex-col flex-1 gap-9 w-full">
+            <ul className='flex flex-col flex-1 gap-9 w-full'>
               {posts?.documents.map((post: Models.Document) => (
                 <PostCard post={post} key={post.caption} />
               ))}
